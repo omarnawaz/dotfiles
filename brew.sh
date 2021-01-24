b6d98e68b24c018e9d69fb5a3bd6d36fccc93025
Omar Nawaz <muna9068@colorado.edu>
2021-01-24 06:38:50 -0700

    Second Commit

--- brew.sh
+++ brew.sh
@@ -1,25 +1,21 @@
 #!/usr/bin/env bash
-
+  
 # Install Brew Packages
 brew install python
 brew install tree
 
 # Install MacOS Applications
-brew cask install google-chrome
-brew cask install firefox
-brew cask install sublime-text
-brew cask install virtualbox
-brew cask install sourcetree
-brew cask install spotify
-brew cask install discord
-brew cask install google-backup-and-sync
-brew cask install skype
-brew cask install gimp
-brew cask install vlc
-brew cask install hyperdock
-brew cask install divvy
+brew install --cask google-chrome
+brew install --cask firefox
+brew install --cask vlc
+brew install --cask zotero
+brew install --cask app-cleaner
+brew install --cask panoply
+brew install --cask stats
+brew install --cask the-unarchiver
+brew install --cask visual-studio-code
+brew install --cask xquartz
 
 # Install Source Code Pro Font
 brew tap homebrew/cask-fonts
-brew cask install font-source-code-pro
-
+brew install --cask font-source-code-pro
