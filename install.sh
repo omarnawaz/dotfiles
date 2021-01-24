ee84dc92e676f13db39e22eae9bddab71af2c7c3
Omar Nawaz <muna9068@colorado.edu>
2021-01-24 06:30:31 -0700

    Initial Commit

--- install.sh
+++ install.sh
@@ -2,7 +2,6 @@
 ############################
 # This script creates symlinks from the home directory to any desired dotfiles in ${homedir}/dotfiles
 # And also installs Homebrew Packages
-# And sets Sublime preferences
 ############################
 
 if [ "$#" -ne 1 ]; then
